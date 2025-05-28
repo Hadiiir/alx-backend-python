54dce475ad16a795712df4416feea53b49fffdf3
Hadiiir <hadeer.gamal3010@gmail.com>
2025-05-29 02:46:14 +0300

    client

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python3
-"""A github org client"""
-from typing import List, Dict
-from utils import get_json, memoize
+"""A github org client
+"""
+from typing import (
+    List,
+    Dict,
+)
+
+from utils import (
+    get_json,
+    access_nested_map,
+    memoize,
+)
+
 
 class GithubOrgClient:
-    """A Github org client"""
+    """A Githib org client
+    """
     ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
-        """Init method"""
+        """Init method of GithubOrgClient"""
         self._org_name = org_name
-        self._org = None
 
-    @property
+    @memoize
     def org(self) -> Dict:
-        """Get org data"""
-        if self._org is None:
-            self._org = get_json(self.ORG_URL.format(org=self._org_name))
-        return self._org
+        """Memoize org"""
+        return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
         """Public repos URL"""
         return self.org["repos_url"]
 
-    @property
+    @memoize
     def repos_payload(self) -> Dict:
-        """Get repos payload"""
+        """Memoize repos payload"""
         return get_json(self._public_repos_url)
 
     def public_repos(self, license: str = None) -> List[str]:
@@ -36,6 +44,7 @@ class GithubOrgClient:
             repo["name"] for repo in json_payload
             if license is None or self.has_license(repo, license)
         ]
+
         return public_repos
 
     @staticmethod
@@ -43,6 +52,7 @@ class GithubOrgClient:
         """Static: has_license"""
         assert license_key is not None, "license_key cannot be None"
         try:
-            return repo["license"]["key"] == license_key
+            has_license = access_nested_map(repo, ("license", "key")) == license_key
         except KeyError:
             return False
+        return has_license
